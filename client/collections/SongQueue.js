d965c06fa6add7a2ddfebd96152fdce748cd421a
delucco <mdelucco@gmail.com>
2014-12-31 17:25:49 -0800

    start basic queues

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,13 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    // this.set('queue', []);
+  },
+
+  addSong: function (song) {
+    // console.log(song.toJSON());
+    // this.get('models').push(song);
+    this.add(new SongModel(song.attributes));
   }
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,28 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+    this.render();
+
+    this.collection.on('add', function(model){
+      console.log('song enqueued');
+      this.render();
+      // this.playerView.setSong(model.get('currentSong'));
+    }, this);
   },
 
-  render: function() {
-    return this.$el;
+  render: function(){
+    // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
+
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
